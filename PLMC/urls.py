01e2b24aee2f9e5b25375797be63041e23f5aec7
dor272 <dor272@gmail.com>
2018-03-29 15:08:02 +0300

    fixed libraries, creates login page and view

--- PLMC/urls.py
+++ PLMC/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from clients import views as a
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', a.login_view),
     url(r'^clients/',include('clients.urls'))
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def index(request):
     return render(request,'base.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+      #  if AuthenticationForm(data=request.POST).is_valid():
+       #     HttpResponse('yes')
+        #else:
+        return render(request,'login.html', {'form': AuthenticationForm(), 'invalid': True})
+    else:
+        return render(request,'login.html', {'form': AuthenticationForm()})
